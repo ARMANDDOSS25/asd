c93463bcf698d076715eda91294554f8c1c7d064
ARMANDDOSS25 <mr.arman.3925@gmail.com>
2024-11-30 19:12:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @MrRanDom8
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7694295301:AAEQF1UR3ceU3JpQlh1snPihVjdUFdlqVq4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("MrRanDom8")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5759284972")
+
+
+
+
+  
